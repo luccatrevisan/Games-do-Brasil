5c30736f1f19d0ae4a177d33a262f856f2d25f64
luccatrevisan <141439341+luccatrevisan@users.noreply.github.com>
2024-09-09 13:52:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,77 @@
+let dados = [
+    {
+        titulo: "Celeste",
+        descricao: "Celeste é um emocionante jogo de plataforma 2D que combina desafios precisos com uma narrativa comovente sobre superação pessoal. Ajude Madeline, uma jovem com ansiedade, a escalar a Montanha Celeste e enfrentar seus medos internos. Com uma jogabilidade desafiadora e refinada, Celeste oferece uma experiência intensa e memorável, repleta de momentos de superação e descoberta.",
+        desenvolvedor: "Matt Makes Games (com colaboração de brasileiros)",
+        genero: "Plataforma",
+        plataformas: ["PC", "Nintendo Switch", "Xbox One", "PlayStation 4"],
+        nota: 9.3,
+        link: "https://store.steampowered.com/app/673720/Celeste/",
+        imagem: "link"
+      },
+      {
+        titulo: "Chacara",
+        descricao: "Chacara é um jogo de terror com estética retrô e gráficos PS1/PSX, ambientado no interior do Brasil. Desenvolvido para oferecer uma experiência assustadora e nostálgica.",
+        desenvolvedor: "David Pateti",
+        genero: "Ação, Terror, Indie",
+        plataformas: "PC",
+        nota: 7.5,
+        link: "https://store.steampowered.com/app/2147640/Chacara/?l=portuguese",
+        tags: "",
+        imagem: "link"
+      },
+      {
+        titulo: "Minit",
+        descricao: "Um jogo de aventura com um ciclo de vida de um minuto, onde você deve explorar um mundo misterioso e descobrir seus segredos.",
+        desenvolvedor: "Jenova Chen e Kitty Calis (com colaboração brasileira)",
+        genero: "Ação e Aventura",
+        plataformas: ["PC", "Nintendo Switch", "PlayStation 4", "Android", "iOS", "Xbox One"],
+        nota: 8.2,
+        link: "https://store.steampowered.com/app/609490/Minit/" ,
+        tags: "",
+        imagem: "link"
+      },
+    {
+        titulo: "Horizon Chase Turbo",
+        descricao: "Um jogo de corrida arcade que evoca a nostalgia dos clássicos dos 16 bits, com gráficos vibrantes e uma trilha sonora contagiante.",
+        desenvolvedor: "Aquiris Game Studio",
+        genero: "Corrida",
+        plataformas: ["PC", "Nintendo Switch", "PlayStation 4", "Xbox One", "iOS", "Android"],
+        nota: 8.5,
+        link: "https://store.steampowered.com/app/389140/Horizon_Chase_Turbo/",
+        tags: "",
+        imagem: "link"
+      },
+      {
+        titulo: "Chroma Squad",
+        descricao: "Um RPG tático no qual você cria seu próprio programa de televisão de super heróis, gerenciando a equipe, criando episódios e até mesmo lidando com contratos e patrocínios.",
+        desenvolvedor: "Behold Studios",
+        genero: "RPG Tático",
+        plataformas: ["PC", "Nintendo Switch", "PlayStation 4", "Xbox One"],
+        nota: 8.2,
+        link: "https://store.steampowered.com/app/251130/Chroma_Squad/",
+        tags: "",
+        imagem: "link"
+      },
+      {
+        titulo: "Dandara",
+        descricao: "Um metroidvania com uma estética única e uma jogabilidade desafiadora, ambientado em um mundo em ruínas.",
+        desenvolvedor: "Long Hat House",
+        genero: "Metroidvania",
+        plataformas: ["PC", "Nintendo Switch", "PlayStation 4"],
+        nota: 8.8,
+        link: "https://store.steampowered.com/app/612390/Dandara_Trials_of_Fear_Edition/",
+        tags: "",
+        imagem: "link"
+      },
+      {
+        titulo: "Chicory: A Colorful Tale",
+        descricao: "Um jogo de aventura artístico onde você pinta o mundo com um pincel mágico, resolvendo puzzles e restaurando a cor ao ambiente.",
+        desenvolvedor: "Greg Lobanov (com colaboração brasileira)",
+        genero: "Aventura",
+        plataformas: ["PC", "Nintendo Switch", "PlayStation 4", "Xbox One"],
+        nota: 9.0,
+        tags: "",
+        link: "https://store.steampowered.com/app/1123450/Chicory_A_Colorful_Tale/"
+      }
+];
